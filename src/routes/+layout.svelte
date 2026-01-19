559e461bd4ad305abe57d7155f7d8453148d32af
Malghnei <malikalghneimin@gmail.com>
2026-01-19 14:17:47 -0500

    Deployment setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+export const prerender = true;
 
 {@render children()}
